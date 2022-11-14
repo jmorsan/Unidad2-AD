90e481cb9b0e237fd51e625dbb71d81e44f327fe
jmorsan426 <jmorsan426@g.educaand.es>
2022-11-14 13:23:36 +0100

    Tarea tr16

---
+++ src/main/java/ies/jms/tr16/Alumno.java
@@ -0,0 +1,57 @@
+package ies.jms.tr16;
+
+public class Alumno {
+
+    private String nombre;
+    private int edad;
+    private double calificacion;
+    private boolean unidadesPendientes;
+
+    public Alumno(String nombre, int edad, double calificacion, boolean unidadesPendientes) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.calificacion = calificacion;
+        this.unidadesPendientes = unidadesPendientes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public boolean isUnidadesPendientes() {
+        return unidadesPendientes;
+    }
+
+    public void setUnidadesPendientes(boolean unidadesPendientes) {
+        this.unidadesPendientes = unidadesPendientes;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Nombre: " + nombre +
+                ", Edad: " + edad +
+                ", Calificacion: " + calificacion +
+                ", Unidades Pendientes: " + unidadesPendientes ;
+    }
+}
---
+++ src/main/java/ies/jms/tr16/Tarea.java
@@ -0,0 +1,223 @@
+package ies.jms.tr16;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Tarea {
+
+    private static final Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        List<Alumno> listaAlumnos = new ArrayList<Alumno>();
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+        try
+        {
+
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(new File("alumno.xml"));
+            Element rootElement = document.getDocumentElement();
+            NodeList nodeListStudent= rootElement.getElementsByTagName("alumno");
+            for(int l = 0; l < nodeListStudent.getLength(); l++)
+            {
+                Element nodeStudent = (Element) nodeListStudent.item(l);
+
+                String nombre = nodeStudent.getAttributes().getNamedItem("nombre").getTextContent();
+                int edad= Integer.valueOf(nodeStudent.getAttributes().getNamedItem("edad").getTextContent());
+                double calificaciones= Double.valueOf(nodeStudent.getElementsByTagName("calificacion").item(0).getTextContent());
+                boolean unidadesPendientes= Boolean.valueOf(nodeStudent.getElementsByTagName("unidadesPendientes").item(0).getTextContent());
+
+                System.out.println("Nombre: "+nombre+" ,Edad: "+edad+ " ,calificaciones: "+ calificaciones+ " , Unidades Pendientes: "+unidadesPendientes);
+
+                Alumno alumno = new Alumno(nombre,edad,calificaciones,unidadesPendientes);
+
+                listaAlumnos.add(alumno);
+
+
+            }
+
+            alumnosPendiente(listaAlumnos);
+            alumnosMaxMinNota(listaAlumnos);
+            alumnosEdadMedia(listaAlumnos);
+            alumnosNotaMedia(listaAlumnos);
+
+
+        }
+        catch (ParserConfigurationException | SAXException | IOException xmlException)
+        {
+            xmlException.printStackTrace();
+
+        }
+    }
+
+    private static void alumnosPendiente(List<Alumno>listaAlumnos) {
+        FileWriter fileWriter = null;
+        PrintWriter printWriter = null;
+
+        try {
+            fileWriter = new FileWriter("AlumnosPendientes.txt");
+            printWriter = new PrintWriter(fileWriter);
+
+            listaAlumnos.sort(Comparator.comparing(Alumno::getNombre));
+
+            for(Alumno alumno : listaAlumnos){
+                if(!alumno.isUnidadesPendientes()){
+                    printWriter.println(alumno);
+                }
+            }
+
+
+            printWriter.flush();
+
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        }
+    }
+
+    private static void alumnosMaxMinNota(List<Alumno>listaAlumnos) {
+        FileWriter fileWriter = null;
+        PrintWriter printWriter = null;
+
+        try {
+            fileWriter = new FileWriter("AlumnoMejorPeorNota.txt");
+            printWriter = new PrintWriter(fileWriter);
+
+            listaAlumnos.sort(Comparator.comparing(Alumno::getCalificacion));
+            printWriter.println("Peor Alumno:");
+            printWriter.println(listaAlumnos.get(0).toString());
+
+            listaAlumnos.sort(Comparator.comparing(Alumno::getCalificacion).reversed());
+
+            printWriter.println("Mejor Alumno:");
+            printWriter.println(listaAlumnos.get(0).toString());
+
+            printWriter.flush();
+
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        }
+    }
+
+    private static void alumnosEdadMedia(List<Alumno>listaAlumnos) {
+        FileWriter fileWriter = null;
+        PrintWriter printWriter = null;
+
+        try {
+            fileWriter = new FileWriter("AlumnoMediaEdad.txt");
+            printWriter = new PrintWriter(fileWriter);
+            int contador = 0;
+            int edadTotal = 0;
+            for(int i=0; i<listaAlumnos.size();i++)
+            {
+                contador++;
+                edadTotal += listaAlumnos.get(i).getEdad();
+
+
+            }
+            printWriter.println("Media de edad : "+edadTotal/contador);
+
+            printWriter.flush();
+
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        }
+    }
+
+    private static void alumnosNotaMedia(List<Alumno>listaAlumnos) {
+        FileWriter fileWriter = null;
+        PrintWriter printWriter = null;
+
+        try {
+            fileWriter = new FileWriter("AlumnoMediaNota.txt");
+            printWriter = new PrintWriter(fileWriter);
+
+            int notaTotal = 0;
+            for(int i=0; i<listaAlumnos.size();i++)
+            {
+
+                notaTotal += listaAlumnos.get(i).getCalificacion();
+
+
+            }
+            printWriter.println("Media de nota : "+ notaTotal/listaAlumnos.size());
+
+            printWriter.flush();
+
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        }
+    }
+
+
+
+}
+
+
+
